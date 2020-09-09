bc38d3208bc94f13fc465e35b35474077638206f
fadam00 <adamesfra00@gmail.com>
2020-09-09 09:33:44 -0400

    const

---
+++ Objects/const.js
@@ -0,0 +1,16 @@
+let isRaining = true;
+
+isRaining = false;
+
+console.log(isRaining);
+
+// the const variable does not allow you to reassign a variable. Best used when you know a variable will never be reassigned.
+
+const person = {
+    age: 27
+}
+
+person.age = 28 // Valid
+// person = {} // Invalid
+
+//Why use const? Code
